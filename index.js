55261756ad67ddb2c848e85ac174ea7d548f3a08
Kelley Shackelford <kelleyalexis@yahoo.com>
2024-03-07 18:14:18 -0500

    Wrote functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - 42);
+}
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+function calculatesFarePrice(start, destination){
+    const distance = Math.abs(start - destination) * 264;
+    if (distance < 400){
+        return 0;
+    }
+    else if (distance < 2000){
+        return (distance - 400) * 0.02 
+    }
+    else if (distance >= 2000 && distance < 2500) {
+    return 25;
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
